ae2efe6ffc6d698d689fe3e973c2cc0fe98af6c2
Christian Potvin <potvinc@gmail.com>
2020-09-21 08:17:27 -0400

    Button Toggling Data Views - Toggling between data sets.

--- app/components/chart.js
+++ app/components/chart.js
@@ -1,33 +1,52 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ChartComponent extends Component {
-    get results() {
+
+    @tracked isPostBodyLengthChart = false;
+
+    @action toggleChart() {
+        this.isPostBodyLengthChart = !this.isPostBodyLengthChart;
+    }
+
+    get buttonTitle() {
+        return this.isPostBodyLengthChart ? 'View Post Title Length Chart' : 'View Post Body Length Chart';
+    }
+
+    get highlow() {
         let { entries } = this.args;
 
-        const MAX_INTERVAL_ENTRIES = 10;
-
-        let results = new Array(entries.length);
-
-        let labels = new Array();
-        let data = new Array();
+        let initial = new Array(entries.length);
 
         let highest = Number.MIN_SAFE_INTEGER;
         let lowest = Number.MAX_SAFE_INTEGER;
 
-        for (let i = 0; i < results.length; i++)
+        for (let i = 0; i < initial.length; i++)
         {
-            results[i] = entries[i].title.length;
+            initial[i] = this.isPostBodyLengthChart ? entries[i].body.length : entries[i].title.length
 
-            if (results[i] > highest)
+            if (initial[i] > highest)
             {
-                highest = results[i];
+                highest = initial[i];
             }
 
-            if (results[i] < lowest)
+            if (initial[i] < lowest)
             {
-                lowest = results[i];
+                lowest = initial[i];
             }
         }
+
+        return { initial, lowest, highest };
+    }
+
+    get results() {
+        let { initial, lowest, highest} = this.highlow;
+
+        const MAX_INTERVAL_ENTRIES = 10;
+
+        let labels = new Array();
+        let data = new Array();
 
         let interval = Math.ceil((highest - lowest) / MAX_INTERVAL_ENTRIES);
         
@@ -42,11 +61,11 @@ export default class ChartComponent extends Component {
             data.push(0);
         }
 
-        for (let i = 0; i < results.length; i++)
+        for (let i = 0; i < initial.length; i++)
         {
-            results[i] = Math.floor((results[i] - lowest) / interval);
+            initial[i] = Math.floor((initial[i] - lowest) / interval);
 
-            data[results[i]] += 1;
+            data[initial[i]] += 1;
         }
 
         return { labels, data };
@@ -54,18 +73,15 @@ export default class ChartComponent extends Component {
 
     get chartData() {
         let { labels, data } = this.results;
-        let { label } = this.args;
-
-        console.log(labels);
-        console.log(data);
+        let label = this.isPostBodyLengthChart ? 'Post Body Length' : 'Post Title Length';
 
         let result = {
             labels: labels,
             datasets: [
                 {
                     label: label,
-                    backgroundColor: 'rgba(50, 50, 50, 0.1)',
-                    borderColor: 'rgba(10, 10, 10, 0.1)',
+                    backgroundColor: 'rgba(152,251,152, 0.5)',
+                    borderColor: 'rgba(152,251,152, 0.5)',
                     borderWidth: 1,
                     data: data
 				}]
